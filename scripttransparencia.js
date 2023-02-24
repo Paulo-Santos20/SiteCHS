bccab14c0731a2f8545f72a7fd261db9a48ce2d2
Paulo <paulo_santos20@outlook.com.br>
2023-02-24 15:50:32 -0300

    transparency page

--- scripttransparencia.js
+++ scripttransparencia.js
@@ -1,12 +1,20 @@
-const topics = document.querySelectorAll('.topic');
+var sublistas = document.querySelectorAll('.secundary-container ul ul');
+var listaPrincipal = document.querySelectorAll('.secundary-container ul > li');
 
-topics.forEach(topic => {
-  topic.addEventListener('click', function() {
-    const subTopic = this.querySelector('.sub-topic');
-    if (subTopic.style.display === 'none') {
-      subTopic.style.display = 'block';
+for (var i = 0; i < listaPrincipal.length; i++) {
+  listaPrincipal[i].addEventListener('click', function() {
+    var sublista = this.querySelector('ul');
+    if (!sublista) return;
+    for (var j = 0; j < sublistas.length; j++) {
+      if (sublistas[j] != sublista) {
+        sublistas[j].style.display = 'none';
+      }
+    }
+    if (sublista.style.display == 'none') {
+      sublista.style.display = 'block';
     } else {
-      subTopic.style.display = 'none';
+      sublista.style.display = 'none';
     }
   });
-});
+}
+
