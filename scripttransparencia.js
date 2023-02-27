2c1ebdb8fc1e9b943b2894b46ad8f62b01069442
Paulo <paulo_santos20@outlook.com.br>
2023-02-27 16:32:00 -0300

    insues transparency

--- scripttransparencia.js
+++ scripttransparencia.js
@@ -1,20 +1,12 @@
-var sublistas = document.querySelectorAll('.secundary-container ul ul');
-var listaPrincipal = document.querySelectorAll('.secundary-container ul > li');
-
-for (var i = 0; i < listaPrincipal.length; i++) {
-  listaPrincipal[i].addEventListener('click', function() {
-    var sublista = this.querySelector('ul');
-    if (!sublista) return;
-    for (var j = 0; j < sublistas.length; j++) {
-      if (sublistas[j] != sublista) {
-        sublistas[j].style.display = 'none';
-      }
-    }
-    if (sublista.style.display == 'none') {
-      sublista.style.display = 'block';
-    } else {
-      sublista.style.display = 'none';
-    }
+document.querySelectorAll(".sub-list-toggle").forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    // Encontra a sub-lista correspondente
+    const subList = toggle.nextElementSibling;
+    // Adiciona ou remove a classe "expanded" para expandir ou ocultar a sub-lista
+    subList.classList.toggle("expanded");
+    // Atualiza o texto do botão
+    toggle.textContent = subList.classList.contains("expanded") ? "Ocultar" : "Expandir";
+    // Atualiza a classe do botão
+    toggle.classList.toggle("expanded");
   });
-}
-
+});
